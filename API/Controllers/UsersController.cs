843a74feca3cb1f5d1d6b390c8e75ef7a72ca390
Alexander <dodecad@outlook.com>
2018-04-26 23:36:02 +0300

    API project

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/users")]
+    public class UsersController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            // Please make sure that you register one of the services in the Startup class:
+            _userService = userService;
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult Get(int id)
+        {
+            var user = _userService.GetUser(id);
+            return Ok(user);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var users = _userService.ListUsers();
+            return Ok(users);
+        }
+    }
+}
---
+++ API/Startup.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Contracts;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            // Register application services:
+
+            // Uncomment the appropriate UserService implementation:
+            // services.AddTransient<IUserService, Domain.Services.UserService>();
+            // services.AddTransient<IUserService, Domain.Services.Fake.UserService>();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
